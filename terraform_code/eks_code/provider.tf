19702b074a5b8a68070751eeda9d3aa3c026f1a2
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2024-10-06 12:26:59 +0530

    Amazon-Prime-Project

---
+++ terraform_code/eks_code/provider.tf
@@ -0,0 +1,16 @@
+locals {
+  region = "us-east-1"
+  name   = "devops-project-cluster"
+  vpc_cidr = "10.0.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+  tags = {
+    Example = local.name
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
