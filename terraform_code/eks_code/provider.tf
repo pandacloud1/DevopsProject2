7eb6dbc9fa6dabc8fa2c2bf39672d8ad2babaef6
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2024-10-06 19:46:10 +0530

    Update provider.tf

--- terraform_code/eks_code/provider.tf
+++ terraform_code/eks_code/provider.tf
@@ -1,6 +1,6 @@
 locals {
   region = "us-east-1"
-  name   = "devops-project-cluster"
+  name   = "amazon-prime-cluster"
   vpc_cidr = "10.0.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
