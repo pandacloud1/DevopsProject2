067a6ff48f5f555607db422607f34c87ca8ccb7c
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2024-10-18 18:32:54 +0530

    Create access.sh

---
+++ access.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ArgoCD Access
+argo_url=$(kubectl get svc -n argocd | grep argocd-server | awk '{print$4}' | head -n 1)
+argo_initial_password=$(argocd admin initial-password -n argocd)
+
+# ArgoCD Credentials
+argo_user="admin"
+
+argo_password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
+
+# Prometheus and Grafana URLs and credentials
+prometheus_url=$(kubectl get svc -n prometheus | grep stable-kube-prometheus-sta-prometheus | awk '{print $4}')
+grafana_url=$(kubectl get svc -n prometheus | grep stable-grafana | awk '{print $4}')
+grafana_user="admin"
+grafana_password=$(kubectl get secret stable-grafana -n prometheus -o jsonpath="{.data.admin-password}" | base64 --decode)
+
+# Print or use these variables
+echo "------------------------"
+echo "ArgoCD URL: $argo_url"
+echo "ArgoCD User: $argo_user"
+echo "ArgoCD Initial Password: $argo_initial_password" | head -n 1
+echo
+echo "Prometheus URL: $prometheus_url":9090
+echo
+echo "Grafana URL: $grafana_url"
+echo "Grafana User: $grafana_user"
+echo "Grafana Password: $grafana_password"
+echo "------------------------"
+
+# Run below commands
+# chmod a+x access.sh
+# ./access.sh
